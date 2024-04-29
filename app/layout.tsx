a1c8e2a05def3e0d2eedd4880f9a631ecd08f84e
Sebastian <sebastian.sanchezisame@outlook.com>
2024-04-29 09:30:37 +0200

    Add header in the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat  } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components";
 
 const monserrat = Montserrat({
   variable: "--font-monserrat",
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         <div className="bg-divLeftBlur absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] ronded-full blur-[10rem] sm:w-[68.75]" ></div>
         <div className="bg-divRightBlur absolute top-[-1rem] left-[35rem] h-[31.25rem] w-[50rem] ronded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
       </body>
     </html>
