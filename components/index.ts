a2d5b20b77d852035dc473506dece96a9e6ddbf3
Sebastian <sebastian.sanchezisame@outlook.com>
2024-04-29 16:27:41 +0200

    Create project section

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 export * from './about';
 export * from './header';
 export * from './intro';
+export * from './projects';
 export * from './sectionDivider';
 export * from './sectionHeading';
---
+++ components/projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SectionHeading } from './sectionHeading'
+
+type Props = {}
+
+export function Projects({}: Props) {
+  return (
+    <section>
+        <SectionHeading>My Projects</SectionHeading>
+    </section>
+  )
+}
